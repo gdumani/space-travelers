0b46f5fc37e870d07bd1d5de7d5f0a3053f17463
Juank628 <jsanchez_zunino@yahoo.com>
2023-01-31 10:17:38 -0500

    Nabvar basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './App.css';
 
 export default function App() {
   return (
     <BrowserRouter>
-      Navbar
+      <Navbar />
       <Routes />
     </BrowserRouter>
   );
