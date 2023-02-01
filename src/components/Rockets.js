2bd3954aecd1cba5435008af7c862d88a0445235
gdumani <gdumani@gmail.com>
2023-02-01 08:04:36 -0600

    create route for rockets, render rocket files

---
+++ src/components/Rockets.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Rocket from './Rocket';
+
+const Rockets = () => (
+  <div>
+    <h2>Rockets</h2>
+    <Rocket />
+  </div>
+);
+
+export default Rockets;
---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,12 @@
+const initialState = {}
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+
+  case first:
+    return { ...state, ...payload }
+
+  default:
+    return state
+  }
+}
