524f69f08789ed0e1c34f7dc195078b1fac07326
Juank628 <jsanchez_zunino@yahoo.com>
2023-02-02 19:27:39 -0500

    store conflict fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
 import App from './App';
-import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
