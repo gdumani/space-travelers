65c22156843c0bf94edefc2706f6e5f1adee4cad
Juan Carlos Sánchez <jsanchez_zunino@yahoo.com>
2023-02-02 19:24:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 
